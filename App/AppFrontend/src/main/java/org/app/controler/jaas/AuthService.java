1fec8e846f9b6f24669ef50ccd081875d993ec01
Hans-Georg Glöckler <h.g.gloeckler@gmx.de>
2018-08-17 08:22:30 +0200

    demo

--- App/AppFrontend/src/main/java/org/app/controler/jaas/AuthService.java
+++ App/AppFrontend/src/main/java/org/app/controler/jaas/AuthService.java
@@ -130,12 +130,12 @@ public class AuthService {
 		return MessageForAuthentication;
 	}
 
-	public Account getAccount() {
-		return account;
-	}
-
-	public void setAccount(Account account) {
-		this.account = account;
-	}
+//	public Account getAccount() {
+//		return account;
+//	}
+//
+//	public void setAccount(Account account) {
+//		this.account = account;
+//	}
 
 }
